00a9327548fac4d5bd4d2fee6e2fd67b295604a9
Ruan Pablo Medeiros <pm.ruan@gmail.com>
2022-04-17 23:55:02 -0300

    fix: add lint configurations

--- src/domain/useCases/authentication.ts
+++ src/domain/useCases/authentication.ts
@@ -1,9 +1,9 @@
 import { AccountModel } from '../models/account-model'
 
 type AuthenticatioParams = {
-  email: string,
+  email: string
   password: string
-} 
+}
 
 export interface Authentication {
   auth (params: AuthenticatioParams): Promise<AccountModel>
